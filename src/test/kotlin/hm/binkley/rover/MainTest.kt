da7271b0ac04c74889b2c3ed2c9c0e805ab13663
Brian Oxley <boxley@thoughtworks.com>
2021-02-27 11:14:54 +0800

    More idiomatic

--- src/test/kotlin/hm/binkley/rover/MainTest.kt
+++ src/test/kotlin/hm/binkley/rover/MainTest.kt
@@ -12,26 +12,24 @@ internal class MainTest {
 
     @Test
     fun `should steer math-style`() = Mathy::main.assertInAndOut()
-
-    companion object {
-        private val inLines = lines("/input")
-        private val expectedOutLines = lines("/output")
-
-        private fun ((Array<out String>) -> Unit).assertInAndOut() =
-            tapSystemOutNormalized {
-                assertNothingWrittenToSystemErr {
-                    withTextFromSystemIn(*inLines.toTypedArray()).execute {
-                        this(emptyArray())
-                    }
-                }
-            }.realLines() shouldBe expectedOutLines
-
-        private fun lines(resourcePath: String) =
-            requireNotNull(this::class.java.getResource(resourcePath))
-                .readText()
-                .realLines()
-    }
 }
+
+private val inLines = lines("/input")
+private val expectedOutLines = lines("/output")
+
+private fun ((Array<out String>) -> Unit).assertInAndOut() =
+    tapSystemOutNormalized {
+        assertNothingWrittenToSystemErr {
+            withTextFromSystemIn(*inLines.toTypedArray()).execute {
+                this(emptyArray())
+            }
+        }
+    }.realLines() shouldBe expectedOutLines
+
+private fun lines(resourcePath: String) =
+    requireNotNull(MainTest::class.java.getResource(resourcePath))
+        .readText()
+        .realLines()
 
 // Ignore unreal "line" after terminal newline
 private fun String.realLines() = lines().dropLast(1)
