f318459b8c2ef6a91c5d3ebeb4d625e96287e1ca
Brian Oxley <boxley@thoughtworks.com>
2021-03-08 10:10:41 +0800

    More idiomatic

--- src/main/kotlin/hm/binkley/rover/rover.kt
+++ src/main/kotlin/hm/binkley/rover/rover.kt
@@ -5,13 +5,13 @@ package hm.binkley.rover
  *
  * The "index" property is suitable for reporting input line numbers
  */
-internal fun inputLines(): Iterable<IndexedValue<String>> {
-    return generateSequence(::readLine)
+internal fun inputLines(): Iterable<IndexedValue<String>> =
+    generateSequence(::readLine)
         .toList()
         .withIndex()
         .map {
+            // Present 1-based indexing for line numbers, not 0-based
             IndexedValue(it.index + 1, it.value)
         }
-}
 
 internal val space = " ".toRegex()
